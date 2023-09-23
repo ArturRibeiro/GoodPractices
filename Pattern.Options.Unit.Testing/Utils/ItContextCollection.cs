9c1ba26751baa32be687cdf31b881cf0b2872b77
Ribeiro <arrb@gft.com>
2023-09-23 19:44:00 -0300

    implementation Option pattern validation

---
+++ Pattern.Options.Unit.Testing/Utils/ItContextCollection.cs
@@ -0,0 +1,8 @@
+namespace Pattern.Options.Unit.Testing.Utils
+{
+    [CollectionDefinition("IntegrationTestWebAppFactory")]
+    public class ItContextCollection : ICollectionFixture<IntegrationTestWebAppFactory>
+    {
+
+    }
+}
