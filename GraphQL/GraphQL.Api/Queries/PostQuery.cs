f63912517be10a71ea8671fa4ec92945107446f5
Ribeiro <arrb@gft.com>
2023-11-15 18:08:28 -0300

    Implements query

--- GraphQL/GraphQL.Api/Queries/PostQuery.cs
+++ GraphQL/GraphQL.Api/Queries/PostQuery.cs
@@ -3,7 +3,12 @@ namespace GraphQL.Api.Queries;
 [ExtendObjectType("Query")]
 public record PostQuery
 {
-    public IEnumerable<Post> Posts([Service] ApplicationDbContext service) => service.Posts.ToList();
+    public IEnumerable<Post> Posts(
+        [Service] ApplicationDbContext service)
+        => service.Posts.ToList();
 
-    public Post Post([Service] ApplicationDbContext service, int id) => service.Posts.Find(id);
+    public Post Post(
+        [Service] ApplicationDbContext service,
+        int id)
+        => service.Posts.Find(id);
 }
--- GraphQL/GraphQL.Api/Queries/UserQuery.cs
+++ GraphQL/GraphQL.Api/Queries/UserQuery.cs
@@ -3,7 +3,12 @@ namespace GraphQL.Api.Queries;
 [ExtendObjectType("Query")]
 public record UserQuery
 {
-    public IEnumerable<User> Users([Service] ApplicationDbContext service) => service.Users.ToList();
+    public IEnumerable<User> Users(
+        [Service] ApplicationDbContext context)
+        => context.Users.ToList();
 
-    public User User([Service] ApplicationDbContext service, int id) => service.Users.Find(id);
+    public User User(
+        [Service] ApplicationDbContext context,
+        int id)
+        => context.Users.Find(id);
 }
