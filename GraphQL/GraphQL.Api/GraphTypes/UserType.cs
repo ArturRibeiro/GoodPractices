2c6331d46b994acbde972592469e80e5a0273e21
Ribeiro <arrb@gft.com>
2023-11-20 18:01:20 -0300

    Finalizando a implementação de grapql

--- GraphQL/GraphQL.Api/GraphTypes/UserType.cs
+++ GraphQL/GraphQL.Api/GraphTypes/UserType.cs
@@ -7,11 +7,11 @@ public class UserType : ObjectType<User>
     protected override void Configure(IObjectTypeDescriptor<User> descriptor)
     {
         descriptor.Description("Graphql : User Type");
-        
-        descriptor.Ignore(u => u.Id);
+        descriptor.Ignore(u => u.Password);
+
+        descriptor.Field(u => u.Id);
         descriptor.Field(u => u.Email);
         descriptor.Field(u => u.Name);
-        descriptor.Ignore(u => u.Password);
 
         descriptor
             .Field(o => o.Posts)
