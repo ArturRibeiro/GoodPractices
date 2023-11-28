ef3dfa3de8cdbac953c78b9ef2df2d3be3b8cb9c
Ribeiro <arrb@gft.com>
2023-11-28 18:59:32 -0300

    Adding configuration graphql ...

---
+++ CQRS/Ecommerce/Shop.Api/Startup.cs
@@ -0,0 +1,50 @@
+namespace Shop.Api;
+
+public class Startup
+{
+    private readonly WebApplicationBuilder _builder;
+    private WebApplication _app;
+
+    public Startup(WebApplicationBuilder builder) => _builder = builder;
+
+    public Startup AddServices()
+    {
+        _builder.Services.AddControllers();
+        _builder.Services.AddEndpointsApiExplorer();
+        return this;
+    }
+
+    public Startup AddServiceGraphQL()
+    {
+        _builder.Services
+            .AddGraphQLServer()
+            .AddQueryType(d => d.Name("Query"))
+            .AddMutationType(d => d.Name("Mutation"))
+            .AddProjections()
+            .AddFiltering()
+            //.RegisterDbContext<ApplicationDbContext>()
+            ;
+        return this;
+    }
+
+    public Startup AddConfigure()
+    {
+        var app = _builder.Build();
+        if (app.Environment.IsDevelopment()) { }
+        app.UseHttpsRedirection();
+        app.UseAuthorization();
+        app.MapControllers();
+        app.MapGraphQL();
+        app.UseGraphQLVoyager("/ui/voyager");
+
+        _app = app;
+
+        return this;
+    }
+
+    public Startup Run()
+    {
+        _app.Run();
+        return this;
+    }
+}
